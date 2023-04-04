e56cede77494e736f6e6f060e2baeeb1c6ca47bb
Spypugg <104601807+Spypugg@users.noreply.github.com>
2023-04-04 12:08:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 // Declare Variables
+let cps2 = 0
+let ainput = 0;
+
+// Main Functions and Event Listners
+
 // loop thing
 setInterval(clock, 1000);
-import { cps } from "./effects";
 let numCookie = 0;
+
 function clock() {
-  cps += 0.1;
-  let output = numCookie + cps;
-  document.getElementById("total").innerHTML = output.toFixed(1);
-  console.log(`${cps}`);
+
+    document
+  .getElementById("grandma-upgrade")
+  .addEventListener("click", grandmaUpgradeClicked);
+
+  document
+  .getElementById("cursor-upgrade")
+  .addEventListener("click", cursorUpgradeClicked);
+
+    function grandmaUpgradeClicked() {
+        cps2 += 1;
+      }
+    
+      function cursorUpgradeClicked() {
+        cps2 += 0.1;
+      }
+  cps2 += ainput;
+  let output = numCookie + cps2;
+  let moutput = output.toFixed(1);
+  document.getElementById("jar").innerHTML = moutput;
 }
