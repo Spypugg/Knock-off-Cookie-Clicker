77096bba7bf62cb9b77ea89dd6faebad3d49e1f2
AydenLarsen <124610038+AydenLarsen@users.noreply.github.com>
2023-04-05 09:25:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 // Declare Variables
-let cps2 = 0
+let cps2 = 0;
 let ainput = 0;
 
 // Main Functions and Event Listners
 
 // loop thing
-setInterval(clock, 1000);
+
 let numCookie = 0;
 
-function clock() {
-
-    document
+document
   .getElementById("grandma-upgrade")
   .addEventListener("click", grandmaUpgradeClicked);
 
-  document
+document
   .getElementById("cursor-upgrade")
   .addEventListener("click", cursorUpgradeClicked);
 
-    function grandmaUpgradeClicked() {
-        cps2 += 1;
-      }
-    
-      function cursorUpgradeClicked() {
-        cps2 += 0.1;
-      }
-  cps2 += ainput;
-  let output = numCookie + cps2;
-  let moutput = output.toFixed(1);
-  document.getElementById("jar").innerHTML = moutput;
+function grandmaUpgradeClicked() {
+  setInterval(clock, 1000);
+  function clock() {
+    cps2 += 1;
+    cps2 += ainput;
+    let output = numCookie + cps2;
+    let moutput = output.toFixed(1);
+    document.getElementById("jar").innerHTML = moutput;
+  }
+}
+
+function cursorUpgradeClicked() {
+  setInterval(clock, 1000);
+  function clock() {
+    cps2 += 0.1;
+    cps2 += ainput;
+    let output = numCookie + cps2;
+    let moutput = output.toFixed(1);
+    document.getElementById("jar").innerHTML = moutput;
+  }
 }
