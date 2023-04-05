db817ade1b55bf0b824e3e8a1ee51b6a172e628d
Spypugg <104601807+Spypugg@users.noreply.github.com>
2023-04-05 12:05:01 -0600

    Update main.js

--- main.js
+++ main.js
@@ -24,26 +24,32 @@ function btnClicked() {
   let output = numCookie + cps2;
   let moutput = output.toFixed(1);
   document.getElementById("jar").innerHTML = moutput;
+  console.log(numCookie);
 }
 
 function grandmaUpgradeClicked() {
-  setInterval(clock, 1000);
-  function clock() {
-    cps2 += 1;
+  if (numCookie >= 100) {
+    setInterval(clock, 1000);
+    function clock() {
+      numCookie += 1;
     cps2 += ainput;
-    let output = numCookie + cps2;
-    let moutput = output.toFixed(1);
-    document.getElementById("jar").innerHTML = moutput;
+    let output = numCookie.toFixed(1);;
+    document.getElementById("jar").innerHTML = output;
+    console.log(`Cookies = ${numCookie}`);
+    } 
+  } else {
+    console.log(`You Don't Have Enough Cookies in The Cookie jar`);
+    alert(`You Don't Have Enough Cookies in The Cookie jar`);
   }
 }
 
 function cursorUpgradeClicked() {
   setInterval(clock, 1000);
   function clock() {
-    cps2 += 0.1;
+    numCookie += 0.1;
     cps2 += ainput;
-    let output = numCookie + cps2;
-    let moutput = output.toFixed(1);
-    document.getElementById("jar").innerHTML = moutput;
+    let output = numCookie.toFixed(1);;
+    document.getElementById("jar").innerHTML = output;
+    console.log(`Cookies = ${numCookie}`);
   }
 }
