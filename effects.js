81fa540f7367e1f7dc1b1919b8d2a29eda71a7aa
Spypugg <104601807+Spypugg@users.noreply.github.com>
2023-04-06 09:27:20 -0600

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,1 +1,16 @@
+document.getElementById("btn").addEventListener("mousedown", cookieClickeddown);
+document.getElementById("btn").addEventListener("mouseup", cookieClickedup);
 
+function cookieClickeddown() {
+  document.getElementById("btn").style.backgroundImage =
+    "url(img/crushedCookie.jpg)";
+  document.getElementById("btn").width = "200";
+  document.getElementById("btn").height = "200";
+}
+
+function cookieClickedup() {
+  document.getElementById("btn").style.backgroundImage =
+    "url(img/notCrushedCookie.jpg)";
+  document.getElementById("btn").width = "200";
+  document.getElementById("btn").height = "200";
+}
