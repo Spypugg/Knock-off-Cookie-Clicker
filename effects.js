cd98b0fd8eb11172f485096e404737ed28db72bc
Spypugg <104601807+Spypugg@users.noreply.github.com>
2023-04-14 09:59:46 -0600

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,5 +1,25 @@
+// Event Listners
 document.getElementById("btn").addEventListener("mousedown", cookieClickeddown);
 document.getElementById("btn").addEventListener("mouseup", cookieClickedup);
+document.getElementById("apply").addEventListener("click", applyClicked);
+// Options Value Input
+
+// Declare Variables
+var myAudio = document.querySelector("#audio");
+
+function applyClicked() {
+  let BM = document.getElementById("backgroundMusic").value;
+  // Background Music
+  if (BM === "yesBM") {
+    myAudio.play();
+    console.log(`yes to background music`);
+  } else if (BM === "noBM") {
+    myAudio.pause();
+    console.log(`no to backgroundmusic`);
+  }
+}
+
+// Image Changer for Cookie Clicker
 
 function cookieClickeddown() {
   document.getElementById("btn").style.backgroundImage =
